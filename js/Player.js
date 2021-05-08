44669a29857f8b2ea796bcd3f25d349b371322f8
Nikhil-1309 <76560138+Nikhil-1309@users.noreply.github.com>
2021-05-08 18:38:02 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,36 @@
+class Player {
+  constructor(){
+    this.index = null;
+    this.distance = 0;
+    this.name = null;
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance
+    });
+  }
+  /*this is the static function.objects of the class do not call the static function
+  a class called the static function.here class Player  is called in the static function*/ 
+  static getPlayerInfo(){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers = data.val();
+    })
+  }
+}
